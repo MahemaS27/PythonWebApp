c3989ae58d22a8c87835899593ab468f82d1ae54
Mahema Singh <mahemafire99@gmail.com>
2019-08-18 17:22:37 -0400

    blog category page, views and urls all working

--- blog/views.py
+++ blog/views.py
@@ -11,11 +11,12 @@ def blog_index(request):
   }
   return render(request, "blog_index.html", context)
 
-
-def blog_category(request,category):
+def blog_category(request, category):
   posts = Post.objects.filter(
-      categories_name_contains=category).order_by("-created_on")
-
+      categories__name__contains=category
+  ).order_by(
+      '-created_on'
+  )
   context = {
     "category": category,
     "posts": posts
